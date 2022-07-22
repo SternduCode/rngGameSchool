e1c32328b77b8e5bc1523822d58242a04e5b528d
ICEBL <ICEBL@DESKTOP-5C9OCUS.fritz.box>
2022-07-22 14:41:41 +0200

    Merge branch 'main' of https://github.com/SternduCode/rngGame into main

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -132,8 +132,9 @@ public class TileManager extends Pane {
 			tile.add(new Tile(getClass().getResourceAsStream("/res/tiles/ALSMR.png"), gp));
 			tile.add(new Tile(getClass().getResourceAsStream("/res/tiles/AREU.png"), gp));
 			tile.add(new Tile(getClass().getResourceAsStream("/res/tiles/SandOGR.png"), gp));
-			tile.add(new Tile(getClass().getResourceAsStream("/res/tiles/Hope7.png"), gp));
-
+			tile.add(new Tile(getClass().getResourceAsStream("/res/tiles/Hope7.png"), gp));      
+			tile.add(new Tile(getClass().getResourceAsStream("/res/tiles/A11Haus.png"), gp));    //95
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
