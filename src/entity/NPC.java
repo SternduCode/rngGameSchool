7895593188a2a455854f1c410ffbfadf843ccf74
Sterndu <Sterndu13570@gmail.com>
2022-07-23 16:00:09 +0200

    FritzA

--- src/entity/NPC.java
+++ src/entity/NPC.java
@@ -38,8 +38,9 @@ public class NPC extends ImageView {
 	}
 
 	private List<Image> getAnimatedImages(String path) {
+		System.out.println(path);
 		List<Image> li = new ArrayList<>();
-		Image img = new Image(getClass().getResourceAsStream("/res/player/" + path));
+		Image img = new Image(getClass().getResourceAsStream("/res/npc/" + path));
 		for (int i = 0; i < img.getWidth(); i += origWidth) {
 			WritableImage wi = new WritableImage(img.getPixelReader(), i, 0, origWidth, origHeight);
 			li.add(ImgUtil.resizeImage(wi,
