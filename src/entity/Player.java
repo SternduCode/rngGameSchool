207348c192b0df0ac032c909b5a7c176c6143024
ICEBL <ICEBL@DESKTOP-5C9OCUS.fritz.box>
2022-07-24 18:37:41 +0200

    ka

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -11,7 +11,7 @@ public class Player extends Entity {
 
 	private final SpielPanel gp;
 	private final Input keyH;
-	private final int fps = 5;
+	private final double fps = 7.5;
 
 	public final int screenX;
 	public final int screenY;
