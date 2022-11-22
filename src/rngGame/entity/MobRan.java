161af602e357b6511a893680f639870d9189cc66
neo30 <afd@germany.cdu>
2022-11-22 08:43:22 +0100

    vallah zeilen verringerung

--- src/rngGame/entity/MobRan.java
+++ src/rngGame/entity/MobRan.java
@@ -15,13 +15,11 @@ public class MobRan extends Entity {
 	public MobRan(JsonObject en, GamePanel gp, List<MobRan> entities,
 			ContextMenu cm, ObjectProperty<MobRan> requestor) {
 		super(en, 3*60, gp, "tmob", entities, cm, requestor);
-
 	}
 
 	public MobRan(MobRan en, List< MobRan> entities, ContextMenu cm,
 			ObjectProperty<MobRan> requestor) {
 		super(en, entities, cm, requestor);
-
 	}
 	
 	public void init() {
