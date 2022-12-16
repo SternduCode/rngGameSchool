ef3d8f1fdb075ea25db1cae4766aac30fa9eac6e
neo30 <afd@germany.cdu>
2022-12-16 10:59:11 +0100

    peeeeefh

--- src/rngGame/entity/MobRan.java
+++ src/rngGame/entity/MobRan.java
@@ -1,7 +1,10 @@
 package rngGame.entity;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import com.sterndu.json.JsonObject;
 import javafx.beans.property.ObjectProperty;
@@ -17,11 +20,13 @@ public class MobRan extends Entity {
 	public MobRan(JsonObject en, GamePanel gp, List<MobRan> entities,
 			ContextMenu cm, ObjectProperty<MobRan> requestor) {
 		super(en, 3*60, gp, "npc", entities, cm, requestor);
+		init();
 	}
 	
 	public MobRan(MobRan en, List< MobRan> entities, ContextMenu cm,
 			ObjectProperty<MobRan> requestor) {
 		super(en, entities, cm, requestor);
+		init();
 	}
 	public void init() {
 
@@ -30,7 +35,7 @@ public class MobRan extends Entity {
         });
         getMiscBoxHandler().put("visible", (gpt,self)->{
             Player p = gpt.getPlayer();
-            pathfinding(p.getX(), p.getY(), gpt);
+            Double[] pos = pathfinding(p.getX(), p.getY(), gpt);
         });
     }
 	public Double[] pathfinding (double x, double y,GamePanel sp) {
@@ -42,19 +47,29 @@ public class MobRan extends Entity {
         Stream<PathElement> stream = Stream.of(new PathElement(tileX, tileY, 0));
         for (int i = 0; i < 11; i++) stream=stream.mapMulti((PathElement pe, Consumer<PathElement> out) -> {
             out.accept(pe);
-            if (map.get(pe.y()).get(pe.x()+1).getPoly().getPoints().size()>0) out.accept(new PathElement(pe.x() + 1, pe.y(), pe.distance() + 1));
-            if (map.get(pe.y()).get(pe.x()-1).getPoly().getPoints().size()>0) out.accept(new PathElement(pe.x() - 1, pe.y(), pe.distance() + 1));
-            if (map.get(pe.y()-1).get(pe.x()).getPoly().getPoints().size()>0) out.accept(new PathElement(pe.x(), pe.y() - 1, pe.distance() + 1));
-            if (map.get(pe.y()+1).get(pe.x()).getPoly().getPoints().size()>0) out.accept(new PathElement(pe.x(), pe.y() + 1, pe.distance() + 1));
+            if (map.size()>pe.y() && map.get(pe.y()).size() > pe.x()+1 &&map.get(pe.y()).get(pe.x()+1).getPoly().getPoints().size()>0) out.accept(new PathElement(pe.x() + 1, pe.y(), pe.distance() + 1));
+            if (pe.x()!=0&&map.size()>pe.y() && map.get(pe.y()).size() > pe.x()-1 &&map.get(pe.y()).get(pe.x()-1).getPoly().getPoints().size()>0) out.accept(new PathElement(pe.x() - 1, pe.y(), pe.distance() + 1));
+            if (pe.y()!=0&&map.size()>pe.y()-1 && map.get(pe.y()-1).size() > pe.x() &&map.get(pe.y()-1).get(pe.x()).getPoly().getPoints().size()>0) out.accept(new PathElement(pe.x(), pe.y() - 1, pe.distance() + 1));
+            if (map.size()>pe.y()+1 && map.get(pe.y()+1).size() > pe.x() &&map.get(pe.y()+1).get(pe.x()).getPoly().getPoints().size()>0) out.accept(new PathElement(pe.x(), pe.y() + 1, pe.distance() + 1));
         }).sorted((a, b) -> {
             if (a.x() == b.x()) {
                 if (a.y() == b.y()) return a.distance() < b.distance() ? -1 : 1;
                 else return a.y() < b.y() ? -1 : 1;
             } else return a.x() < b.x() ? -1 : 1;
         }).distinct(); 
+        
+        //System.out.println(stream.collect(Collectors.toList()));
 
-        return null;
+        List<PathElement> pel = stream.collect(Collectors.toList());
+        System.out.println(pel);
+        PathElement pe = pel.get(pel.size()-1);
+        return new Double[] {(double) pe.x(), (double) pe.y()};
     }
+	
+	@Override
+	public void update(long milis) {
+		super.update(milis);
+	}
 
 	
 	
