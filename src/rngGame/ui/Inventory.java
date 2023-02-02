3e688c05597e4a8c7f87a50425160593b33953d3
ICEBL <ICEBL@DESKTOP-5C9OCUS.fritz.box>
2023-02-02 20:37:21 +0100

    banana

--- src/rngGame/ui/Inventory.java
+++ src/rngGame/ui/Inventory.java
@@ -7,6 +7,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import javafx.animation.FadeTransition;
 import javafx.animation.RotateTransition;
 import javafx.animation.TranslateTransition;
+import javafx.scene.Group;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
@@ -19,38 +20,112 @@ import rngGame.tile.ImgUtil;
 
 public class Inventory extends Pane {
 	private final ImageView gamemenu;
+	
+	private final Group buttongroup = new Group();
+	
+	private final ImageView invB;
+	private Image invB1;
+	private Image invB2;
+	
+	private final ImageView queB;
+	private Image queB1;
+	private Image queB2;
+	
+	private final ImageView leavB;
+	private Image leavB1;
+	private Image leavB2;
+	
+	
 	private final GamePanel gamepanel;
 	
 	public Inventory(GamePanel gamepanel) throws FileNotFoundException {
 		gamemenu = new ImageView(ImgUtil.getScaledImage(gamepanel, "./res/gui/gamemenubackround.png"));
+		
+		invB1 = ImgUtil.getScaledImage(gamepanel, "./res/gui/invAbutton1.png");
+		invB2 = ImgUtil.getScaledImage(gamepanel, "./res/gui/invAbutton2.png");
+		invB = new ImageView(invB1);
+		
+		queB1 = ImgUtil.getScaledImage(gamepanel, "./res/gui/queAbutton1.png");
+		queB2 = ImgUtil.getScaledImage(gamepanel, "./res/gui/queAbutton2.png");
+		queB = new ImageView(queB1);
+		
+		leavB1 = ImgUtil.getScaledImage(gamepanel, "./res/gui/LeavAbutton1.png");
+		leavB2 = ImgUtil.getScaledImage(gamepanel, "./res/gui/LeavAbutton2.png");
+		leavB = new ImageView(leavB1);
+		
+		buttongroup.getChildren().addAll(invB,queB,leavB);
+		
 		getChildren().add(gamemenu);
-
+		getChildren().add(buttongroup);
+		
 		gamemenu.setVisible(false);
-		gamemenu.setDisable(true);
+		buttongroup.setVisible(false);
+		
+		this.setDisable(true);
 		this.gamepanel = gamepanel; 
 		
 		AtomicBoolean ab = new AtomicBoolean(false);
 		Input.getInstance().setKeyHandler("inv", mod -> {
 			
 			TranslateTransition ft = new TranslateTransition(Duration.millis(150), gamemenu);
+			TranslateTransition ib1 = new TranslateTransition(Duration.millis(150), buttongroup);
+			
 			if (!ab.getAndSet(!ab.get())) {	
 				gamepanel.setBlockUserInputs(true);
+				this.setDisable(false);
 				gamemenu.setVisible(true);
+				buttongroup.setVisible(true);
+
+				invB.setOnMousePressed(me -> {
+					invB.setImage(invB2);
+				});
+				invB.setOnMouseReleased(me -> {
+					invB.setImage(invB1);
+				});
+				
+				
+				queB.setOnMousePressed(me -> {
+					queB.setImage(queB2);
+				});
+				queB.setOnMouseReleased(me -> {
+					queB.setImage(queB1);
+				});
+				
+				
+				leavB.setOnMousePressed(me -> {
+					leavB.setImage(leavB2);
+				});
+				leavB.setOnMouseReleased(me -> {
+					leavB.setImage(leavB1);
+				});
+				
 				ft.setFromY(gamepanel.SpielHoehe);
+				ib1.setFromY(gamepanel.SpielHoehe);
 				ft.setToY(0);
+				ib1.setToY(0);
 				ft.play();
+				ib1.play();
+
+
 			} else {
-				ft.setFromY(0);
+				
 				ft.setToY(gamepanel.SpielHoehe);
+				ib1.setToY(gamepanel.SpielHoehe);
 				ft.play();
+				ib1.play();
+				
 				gamepanel.setBlockUserInputs(false);
+				
 				new Thread(()->{
 				try {
-					Thread.sleep(250);
+					Thread.sleep(50);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 				gamemenu.setVisible(false);
+				buttongroup.setVisible(false);
+				this.setDisable(true);
+				
 				}).start();
 			}
 		}, KeyCode.TAB, false);
@@ -58,5 +133,17 @@ public class Inventory extends Pane {
 
 	public void f11Scale() {
 		gamemenu.setImage(ImgUtil.getScaledImage(gamepanel, "./res/gui/gamemenubackround.png"));
+		
+		invB1 = ImgUtil.getScaledImage(gamepanel, "./res/gui/invAbutton1.png");
+		invB2 = ImgUtil.getScaledImage(gamepanel, "./res/gui/invAbutton2.png");
+		invB.setImage(invB1);
+		
+		queB1 = ImgUtil.getScaledImage(gamepanel, "./res/gui/queAbutton1.png");
+		queB2 = ImgUtil.getScaledImage(gamepanel, "./res/gui/queAbutton2.png");
+		queB.setImage(queB1);
+		
+		leavB1 = ImgUtil.getScaledImage(gamepanel, "./res/gui/LeavAbutton1.png");
+		leavB2 = ImgUtil.getScaledImage(gamepanel, "./res/gui/LeavAbutton2.png");
+		leavB.setImage(leavB1);
 	}
 }
