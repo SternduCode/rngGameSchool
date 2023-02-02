d4053d8cb5c850a76e9f563b69edaefce0ea13b4
neo30 <afd@germany.cdu>
2023-02-02 13:23:12 +0100

    Merge branch 'main' of https://github.com/SternduCode/rngGame into main

--- src/rngGame/ui/Inventory.java
+++ src/rngGame/ui/Inventory.java
@@ -1,11 +1,62 @@
 package rngGame.ui;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.RotateTransition;
+import javafx.animation.TranslateTransition;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
+import javafx.util.Duration;
+import rngGame.entity.Player;
+import rngGame.main.GamePanel;
+import rngGame.main.Input;
+import rngGame.tile.ImgUtil;
 
 public class Inventory extends Pane {
+	private final ImageView gamemenu;
+	private final GamePanel gamepanel;
+	
+	public Inventory(GamePanel gamepanel) throws FileNotFoundException {
+		gamemenu = new ImageView(ImgUtil.getScaledImage(gamepanel, "./res/gui/gamemenubackround.png"));
+		getChildren().add(gamemenu);
 
-	public Inventory() {
-		// TODO
+		gamemenu.setVisible(false);
+		gamemenu.setDisable(true);
+		this.gamepanel = gamepanel; 
+		
+		AtomicBoolean ab = new AtomicBoolean(false);
+		Input.getInstance().setKeyHandler("inv", mod -> {
+			
+			TranslateTransition ft = new TranslateTransition(Duration.millis(150), gamemenu);
+			if (!ab.getAndSet(!ab.get())) {	
+				gamepanel.setBlockUserInputs(true);
+				gamemenu.setVisible(true);
+				ft.setFromY(gamepanel.SpielHoehe);
+				ft.setToY(0);
+				ft.play();
+			} else {
+				ft.setFromY(0);
+				ft.setToY(gamepanel.SpielHoehe);
+				ft.play();
+				gamepanel.setBlockUserInputs(false);
+				new Thread(()->{
+				try {
+					Thread.sleep(250);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				gamemenu.setVisible(false);
+				}).start();
+			}
+		}, KeyCode.TAB, false);
 	}
 
+	public void f11Scale() {
+		gamemenu.setImage(ImgUtil.getScaledImage(gamepanel, "./res/gui/gamemenubackround.png"));
+	}
 }
