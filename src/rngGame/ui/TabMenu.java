59ab4fb825976be3cd169ec9d10f76cbc986da1f
ICEBL <ICEBL@DESKTOP-5C9OCUS.fritz.box>
2023-02-04 00:05:21 +0100

    Leave button done first try

--- src/rngGame/ui/TabMenu.java
+++ src/rngGame/ui/TabMenu.java
@@ -23,6 +23,8 @@ public class TabMenu extends Pane {
 
 	/** The buttongroup. */
 	private final Group buttongroup = new Group();
+	private final Group YNbuttongroup = new Group();
+
 
 	/** The inv B. */
 	private final ImageView invB;
@@ -50,6 +52,14 @@ public class TabMenu extends Pane {
 
 	/** The leav B 2. */
 	private Image leavB2;
+	
+	private ImageView surebackround;
+	private ImageView sureY;
+	private ImageView sureN;
+	private Image sureY1;
+	private Image sureN1;
+	private Image sureY2;
+	private Image sureN2;
 
 	/** The inventory. */
 	private Inventory inventory;
@@ -57,6 +67,7 @@ public class TabMenu extends Pane {
 	/** The gamepanel. */
 	private final GamePanel gamepanel;
 
+	private AtomicBoolean noInput = new AtomicBoolean(false);
 	/**
 	 * Instantiates a new tab menu.
 	 *
@@ -77,21 +88,40 @@ public class TabMenu extends Pane {
 		leavB1 = ImgUtil.getScaledImage(gamepanel, "./res/gui/LeavAbutton1.png");
 		leavB2 = ImgUtil.getScaledImage(gamepanel, "./res/gui/LeavAbutton2.png");
 		leavB = new ImageView(leavB1);
-
+		
+		surebackround = new ImageView(ImgUtil.getScaledImage(gamepanel, "./res/gui/Sure.png"));
+		
+		sureY1 = ImgUtil.getScaledImage(gamepanel, "./res/gui/SureY.png");
+		sureY2 = ImgUtil.getScaledImage(gamepanel, "./res/gui/SureY2.png");
+		sureY = new ImageView(sureY1);
+		
+		sureN1 = ImgUtil.getScaledImage(gamepanel, "./res/gui/SureN.png");
+		sureN2 = ImgUtil.getScaledImage(gamepanel, "./res/gui/SureN2.png");
+		sureN = new ImageView(sureN1);
+		
+		
 		buttongroup.getChildren().addAll(invB,queB,leavB);
+		YNbuttongroup.getChildren().addAll(sureY, sureN);
+		
 
 		getChildren().add(gamemenu);
 		getChildren().add(buttongroup);
+		getChildren().add(surebackround);
+		getChildren().add(YNbuttongroup);
+		
+		
 
 		gamemenu.setVisible(false);
 		buttongroup.setVisible(false);
+		YNbuttongroup.setVisible(false);
+		surebackround.setVisible(false);
 
 		setDisable(true);
 		this.gamepanel = gamepanel;
 
 		AtomicBoolean ab = new AtomicBoolean(false);
 		Input.getInstance().setKeyHandler("inv", mod -> {
-
+			if(!noInput.get()) {
 			TranslateTransition ft = new TranslateTransition(Duration.millis(150), gamemenu);
 			TranslateTransition ib1 = new TranslateTransition(Duration.millis(150), buttongroup);
 
@@ -130,7 +160,29 @@ public class TabMenu extends Pane {
 				});
 				leavB.setOnMouseReleased(me -> {
 					leavB.setImage(leavB1);
+					noInput.set(true);
+					surebackround.setVisible(true);
+					YNbuttongroup.setVisible(true);
 				});
+				
+				sureN.setOnMousePressed(me -> {
+					sureN.setImage(sureN2);
+				});
+				sureN.setOnMouseReleased(me -> {
+					sureN.setImage(sureN1);
+					noInput.set(false);
+					surebackround.setVisible(false);
+					YNbuttongroup.setVisible(false);
+				});
+				
+				sureY.setOnMousePressed(me -> {
+					sureY.setImage(sureY2);
+				});
+				sureY.setOnMouseReleased(me -> {
+					sureY.setImage(sureY1);
+					System.exit(0);
+				});
+				
 
 				ft.setFromY(gamepanel.SpielHoehe);
 				ib1.setFromY(gamepanel.SpielHoehe);
@@ -164,6 +216,7 @@ public class TabMenu extends Pane {
 
 				}).start();
 			}
+			}
 		}, KeyCode.TAB, false);
 	}
 
@@ -184,6 +237,15 @@ public class TabMenu extends Pane {
 		leavB1 = ImgUtil.getScaledImage(gamepanel, "./res/gui/LeavAbutton1.png");
 		leavB2 = ImgUtil.getScaledImage(gamepanel, "./res/gui/LeavAbutton2.png");
 		leavB.setImage(leavB1);
+		
+		surebackround.setImage(ImgUtil.getScaledImage(gamepanel, "./res/gui/Sure.png"));
+		sureY1 = ImgUtil.getScaledImage(gamepanel, "./res/gui/SureY.png");
+		sureY2 = ImgUtil.getScaledImage(gamepanel, "./res/gui/SureY2.png");
+		sureY.setImage(sureY1);
+		
+		sureN1 = ImgUtil.getScaledImage(gamepanel, "./res/gui/SureN.png");
+		sureN2 = ImgUtil.getScaledImage(gamepanel, "./res/gui/SureN2.png");
+		sureN.setImage(sureN1);
 	}
 
 	/**
