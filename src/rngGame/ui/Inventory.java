0b4f5ebe5d5397eaffe75d4a04ded5c168a918ca
Sterndu <Sterndu13570@gmail.com>
2023-02-03 20:19:38 +0100

    Fixed your shit

--- src/rngGame/ui/Inventory.java
+++ src/rngGame/ui/Inventory.java
@@ -4,27 +4,52 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import rngGame.main.GamePanel;
 import rngGame.tile.ImgUtil;
-import javafx.scene.image.*;
 
 
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Inventory.
+ */
 public class Inventory extends Pane {
+
+	/** The gamepanel. */
 	private final GamePanel gamepanel;
+
+	/** The inv backround. */
 	private final ImageView invBackround;
-	
-	
+
+
+	/**
+	 * Instantiates a new inventory.
+	 *
+	 * @param gamepanel the gamepanel
+	 * @param tabm the tabm
+	 */
 	public Inventory(GamePanel gamepanel, TabMenu tabm) {
 		this.gamepanel = gamepanel;
-		invBackround = new ImageView(ImgUtil.getScaledImage(gamepanel, "./res/gui/gamemenubackround.png"));
+		invBackround	= new ImageView(ImgUtil.getScaledImage(gamepanel, "./res/gui/InvBackround.png"));
 		getChildren().add(invBackround);
 		invBackround.setVisible(false);
-		
-		
+
+
 	}
-	
+
+	/**
+	 * End show.
+	 */
+	public void endShow() {
+		invBackround.setVisible(false);
+	}
+
+	/**
+	 * Show.
+	 */
 	public void show() {
 		invBackround.setVisible(true);
-		System.out.println("banana");
+		invBackround.setTranslateY(0);
+		invBackround.setTranslateX(0);
+		invBackround.setOpacity(1);
 	}
-	
+
 }
--- src/rngGame/ui/TabMenu.java
+++ src/rngGame/ui/TabMenu.java
@@ -1,84 +1,108 @@
 package rngGame.ui;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import javafx.animation.FadeTransition;
-import javafx.animation.RotateTransition;
 import javafx.animation.TranslateTransition;
 import javafx.scene.Group;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.image.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.util.Duration;
-import rngGame.entity.Player;
-import rngGame.main.GamePanel;
-import rngGame.main.Input;
+import rngGame.main.*;
 import rngGame.tile.ImgUtil;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class TabMenu.
+ */
 public class TabMenu extends Pane {
+
+	/** The gamemenu. */
 	private final ImageView gamemenu;
-	
+
+	/** The buttongroup. */
 	private final Group buttongroup = new Group();
-	
+
+	/** The inv B. */
 	private final ImageView invB;
+
+	/** The inv B 1. */
 	private Image invB1;
+
+	/** The inv B 2. */
 	private Image invB2;
-	
+
+	/** The que B. */
 	private final ImageView queB;
+
+	/** The que B 1. */
 	private Image queB1;
+
+	/** The que B 2. */
 	private Image queB2;
-	
+
+	/** The leav B. */
 	private final ImageView leavB;
+
+	/** The leav B 1. */
 	private Image leavB1;
+
+	/** The leav B 2. */
 	private Image leavB2;
-	
+
+	/** The inventory. */
 	private Inventory inventory;
-	
+
+	/** The gamepanel. */
 	private final GamePanel gamepanel;
-	
+
+	/**
+	 * Instantiates a new tab menu.
+	 *
+	 * @param gamepanel the gamepanel
+	 * @throws FileNotFoundException the file not found exception
+	 */
 	public TabMenu(GamePanel gamepanel) throws FileNotFoundException {
 		gamemenu = new ImageView(ImgUtil.getScaledImage(gamepanel, "./res/gui/gamemenubackround.png"));
-		
+
 		invB1 = ImgUtil.getScaledImage(gamepanel, "./res/gui/invAbutton1.png");
 		invB2 = ImgUtil.getScaledImage(gamepanel, "./res/gui/invAbutton2.png");
 		invB = new ImageView(invB1);
-		
+
 		queB1 = ImgUtil.getScaledImage(gamepanel, "./res/gui/queAbutton1.png");
 		queB2 = ImgUtil.getScaledImage(gamepanel, "./res/gui/queAbutton2.png");
 		queB = new ImageView(queB1);
-		
+
 		leavB1 = ImgUtil.getScaledImage(gamepanel, "./res/gui/LeavAbutton1.png");
 		leavB2 = ImgUtil.getScaledImage(gamepanel, "./res/gui/LeavAbutton2.png");
 		leavB = new ImageView(leavB1);
-		
+
 		buttongroup.getChildren().addAll(invB,queB,leavB);
-		
+
 		getChildren().add(gamemenu);
 		getChildren().add(buttongroup);
-		
+
 		gamemenu.setVisible(false);
 		buttongroup.setVisible(false);
-		
-		this.setDisable(true);
-		this.gamepanel = gamepanel; 
-		
+
+		setDisable(true);
+		this.gamepanel = gamepanel;
+
 		AtomicBoolean ab = new AtomicBoolean(false);
 		Input.getInstance().setKeyHandler("inv", mod -> {
-			
+
 			TranslateTransition ft = new TranslateTransition(Duration.millis(150), gamemenu);
 			TranslateTransition ib1 = new TranslateTransition(Duration.millis(150), buttongroup);
-			
-			
-			if (!ab.getAndSet(!ab.get())) {	
+
+
+			if (!ab.getAndSet(!ab.get())) {
 				gamepanel.setBlockUserInputs(true);
-				this.setDisable(false);
+				setDisable(false);
 				gamemenu.setVisible(true);
 				buttongroup.setVisible(true);
-				
-				
+
+
 				invB.setOnMousePressed(me -> {
 					invB.setImage(invB2);
 				});
@@ -87,26 +111,27 @@ public class TabMenu extends Pane {
 					inventory.show();
 					inventory.setDisable(false);
 				});
-				
+
 				inventory = new Inventory(gamepanel, this);
 				getChildren().add(inventory);
+
 				inventory.setDisable(true);
-				
+
 				queB.setOnMousePressed(me -> {
 					queB.setImage(queB2);
 				});
 				queB.setOnMouseReleased(me -> {
 					queB.setImage(queB1);
 				});
-				
-				
+
+
 				leavB.setOnMousePressed(me -> {
 					leavB.setImage(leavB2);
 				});
 				leavB.setOnMouseReleased(me -> {
 					leavB.setImage(leavB1);
 				});
-				
+
 				ft.setFromY(gamepanel.SpielHoehe);
 				ib1.setFromY(gamepanel.SpielHoehe);
 				ft.setToY(0);
@@ -116,44 +141,55 @@ public class TabMenu extends Pane {
 
 
 			} else {
-				
+
 				ft.setToY(gamepanel.SpielHoehe);
 				ib1.setToY(gamepanel.SpielHoehe);
 				ft.play();
 				ib1.play();
-				
+
 				gamepanel.setBlockUserInputs(false);
-				
+
+				inventory.setDisable(true);
+				inventory.endShow();
+
 				new Thread(()->{
-				try {
-					Thread.sleep(50);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				gamemenu.setVisible(false);
-				buttongroup.setVisible(false);
-				this.setDisable(true);
-				
+					try {
+						Thread.sleep(50);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					gamemenu.setVisible(false);
+					buttongroup.setVisible(false);
+					setDisable(true);
+
 				}).start();
 			}
 		}, KeyCode.TAB, false);
 	}
-	
-	public Inventory getInventory() { return inventory; }
 
+	/**
+	 * F 11 scale.
+	 */
 	public void f11Scale() {
 		gamemenu.setImage(ImgUtil.getScaledImage(gamepanel, "./res/gui/gamemenubackround.png"));
-		
+
 		invB1 = ImgUtil.getScaledImage(gamepanel, "./res/gui/invAbutton1.png");
 		invB2 = ImgUtil.getScaledImage(gamepanel, "./res/gui/invAbutton2.png");
 		invB.setImage(invB1);
-		
+
 		queB1 = ImgUtil.getScaledImage(gamepanel, "./res/gui/queAbutton1.png");
 		queB2 = ImgUtil.getScaledImage(gamepanel, "./res/gui/queAbutton2.png");
 		queB.setImage(queB1);
-		
+
 		leavB1 = ImgUtil.getScaledImage(gamepanel, "./res/gui/LeavAbutton1.png");
 		leavB2 = ImgUtil.getScaledImage(gamepanel, "./res/gui/LeavAbutton2.png");
 		leavB.setImage(leavB1);
 	}
+
+	/**
+	 * Gets the inventory.
+	 *
+	 * @return the inventory
+	 */
+	public Inventory getInventory() { return inventory; }
 }
