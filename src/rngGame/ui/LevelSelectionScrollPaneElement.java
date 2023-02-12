094ec76083182acf7792422e8c98e7f4bccd3515
Sterndu <Sterndu13570@gmail.com>
2023-02-12 02:08:15 +0100

    Added a list of all lvl images

--- src/rngGame/ui/LevelSelectionScrollPaneElement.java
+++ src/rngGame/ui/LevelSelectionScrollPaneElement.java
@@ -1,29 +1,57 @@
 package rngGame.ui;
 
+import java.io.*;
+import java.util.stream.*;
+
 import javafx.animation.FadeTransition;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.image.*;
 import javafx.util.Duration;
 import rngGame.buildings.ContractsTable;
-import rngGame.main.GamePanel;
-import rngGame.main.Text;
+import rngGame.main.*;
 import rngGame.tile.ImgUtil;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class LevelSelectionScrollPaneElement.
+ */
 public class LevelSelectionScrollPaneElement extends ScrollPaneElement {
 
+	/** The lvls. */
+	private static Image[] lvls = Stream.of(new File("./res/lvl/vorgeschllagen").list()).map(t -> {
+		try {
+			return new FileInputStream(t);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}).filter(s -> s != null).map(Image::new).collect(Collectors.toList()).toArray(new Image[0]);
+
+	/** The start button. */
 	private final Button background, startButton;
+
+	/** The U gbc. */
 	private final Image UGbc;
-	
-	private ImageView floor1;
+
+	/** The floor 1. */
+	private final ImageView floor1;
+
+	/** The vlvl. */
 	private ImageView vlvl;
 
+	/**
+	 * Instantiates a new level selection scroll pane element.
+	 *
+	 * @param gamepanel the gamepanel
+	 * @param ct the ct
+	 * @param floor the floor
+	 */
 	public LevelSelectionScrollPaneElement(GamePanel gamepanel, ContractsTable ct,String floor) {
 		super(new Button());
 		background = getBackgroundElement();
 		UGbc = ImgUtil.getScaledImage(gamepanel, "./res/Contractstuff/UGbackground.png");
 		Image UGbc2 = ImgUtil.getScaledImage(gamepanel, "./res/Contractstuff/UGbackground2.png");
 		setBackgroundImageToDefaullt();
-		
+
 		Image floor1 = Text.getInstance().convertText(floor, 64);
 		floor1 = ImgUtil.resizeImage(
 				floor1, (int) floor1.getWidth(), (int) floor1.getHeight(),
@@ -31,7 +59,7 @@ public class LevelSelectionScrollPaneElement extends ScrollPaneElement {
 				(int) (floor1.getHeight() * gamepanel.getScalingFactorY()));
 		this.floor1 = new ImageView(floor1);
 		this.floor1.setDisable(true);
-		
+
 		// Start Button
 		Image sButton = ImgUtil.getScaledImage(gamepanel, "./res/Contractstuff/Startbutton.png");
 		Image sButton2 = ImgUtil.getScaledImage(gamepanel, "./res/Contractstuff/Startbutton2.png");
@@ -69,12 +97,23 @@ public class LevelSelectionScrollPaneElement extends ScrollPaneElement {
 		this.floor1.setLayoutY(5*gamepanel.getScalingFactorY());
 	}
 
+	/**
+	 * Gets the start button.
+	 *
+	 * @return the start button
+	 */
 	public Button getStartButton() { return startButton; }
 
+	/**
+	 * Sets the background image to defaullt.
+	 */
 	public void setBackgroundImageToDefaullt() {
 		background.setImage(UGbc);
 	}
-	
+
+	/**
+	 * Start bvisible false.
+	 */
 	public void startBvisibleFalse() {
 		startButton.setVisible(false);
 	}
