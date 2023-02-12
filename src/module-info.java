39e5322fbfac718d1879d871a3ed40455d8efea7
Sterndu <Sterndu13570@gmail.com>
2023-02-12 20:23:20 +0100

    Reverted Fix Gradle Issue

---
+++ src/module-info.java
@@ -0,0 +1,21 @@
+module rngGame {
+	exports rngGame.main;
+	exports rngGame.tile;
+	exports rngGame.entity;
+	exports rngGame.buildings;
+	exports rngGame.ui;
+
+	requires javafx.base;
+	requires transitive javafx.graphics;
+	requires transitive javafx.controls;
+
+	requires javafx.swing;
+	requires transitive java.desktop;
+	requires jdk.zipfs;
+	requires java.xml;
+	requires javafx.media;
+	
+	requires Lib;
+
+	opens rngGame.main to javafx.graphics;
+}
