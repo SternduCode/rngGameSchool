affe87d0200e46ce3f7369350d46019cfb2f8fc3
Sterndu <Sterndu13570@gmail.com>
2023-02-17 21:15:50 +0100

    RARITY

--- src/rngGame/stats/Rarity.java
+++ src/rngGame/stats/Rarity.java
@@ -1,15 +1,45 @@
 package rngGame.stats;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Enum Rarity.
+ */
 public enum Rarity {
-	COMMON,UNCOMMON,RARE,VERYRARE,EPIC,LEGENDARY,GOD,VOID; 
+
+	/** The common. */
+	COMMON,
+	/** The uncommon. */
+	UNCOMMON,
+	/** The rare. */
+	RARE,
+	/** The veryrare. */
+	VERYRARE,
+	/** The epic. */
+	EPIC,
+	/** The legendary. */
+	LEGENDARY,
+	/** The god. */
+	GOD,
+	/** The void. */
+	VOID;
+
+	/** The texture name. */
 	String textureName;
-	
 
 
+
+	/**
+	 * Instantiates a new rarity.
+	 */
 	Rarity() {
-		textureName = toString();
+		textureName = toString().charAt(0) + toString().substring(1).toLowerCase();
 	}
-	
+
+	/**
+	 * Gets the texture name.
+	 *
+	 * @return the texture name
+	 */
 	public String getTextureName() {
 		return textureName;
 	}
