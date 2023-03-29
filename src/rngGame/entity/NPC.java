1918f9ae46f546a1751f6c0ebde50766e7c1f435
Sterndu <57641750+SternduCode@users.noreply.github.com>
2023-03-29 20:09:40 +0000

    Reverted and merged

--- src/rngGame/entity/NPC.java
+++ src/rngGame/entity/NPC.java
@@ -102,8 +102,6 @@ public class NPC extends Entity implements JsonValue {
 	 * Inits the.
 	 */
 	protected void init() {
-		if (!getMiscBoxes().containsKey("talk"))
-			getMiscBoxes().put("talk", new Circle(getReqWidth() / 2, getReqHeight() / 2, 32));
 		collisionBoxes.entrySet().parallelStream()
 		.forEach(s -> {
 			if (s.getValue().getPoints().size() == 0) s.getValue().getPoints().addAll(0d, 0d, 0d,
@@ -114,7 +112,40 @@ public class NPC extends Entity implements JsonValue {
 		});
 		getMiscBoxHandler().put("talk", (gpt,self)->{
 			gpt.getAktionbutton().setInteractionbuttonKann(true, gp2 -> {
-				// Talk
+				try {
+                    gp2.setBlockUserInputs(true);
+                	Image img = new Image(new FileInputStream(new File("./res/gui/bubble/SpeakBubble2.png")));
+                    gamepanel.getBubble().getChildren().add(new ImageView(img));
+                    Random r = new Random();
+                    BufferedReader flr = new BufferedReader(new FileReader(new File("./res/texts/Guenther.txt")));
+                                       
+                    String line = "";
+                    int uff = r.nextInt(5)+1;
+                    try {
+                        switch (uff) {
+                            case 5:
+                                flr.readLine();
+                            case 4:
+                                flr.readLine();
+                            case 3:
+                                flr.readLine();
+                            case 2:
+                                flr.readLine();
+                            case 1:
+                                line=flr.readLine();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                	}
+                    gp2.getBubbleText().setImage(Text.getInstance().convertText(line));
+                    gp2.getBubbleText().setLayoutX((gp2.SpielLaenge/10));
+                    gp2.getBubbleText().setLayoutY((gp2.SpielHoehe/1.4)-(gp2.getBubbleText().getImage().getHeight()/2.0));
+                    System.out.println(gp2.getBubbleText().getImage().getWidth());
+                    System.out.println(line);
+                    System.out.println(uff);                    
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
 			});
 		});
 
