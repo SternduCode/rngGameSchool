650739197aa635839e58640e55ae271f58159c9d
Sterndu <Sterndu13570@gmail.com>
2023-09-10 01:55:12 +0200

    Scaled TitleScreen to WindowSize

--- src/rngGame/main/TitleScreen.java
+++ src/rngGame/main/TitleScreen.java
@@ -164,7 +164,7 @@ public class TitleScreen extends Pane{
 	 * Scale F 11.
 	 */
 	public void scaleF11() {
-		frames = ImgUtil.getScaledImages("./res/backgrounds/Main BG.gif");
+		frames = ImgUtil.getScaledImages("./res/backgrounds/Main BG.gif", WindowManager.getInstance().getBlockSize()*WindowManager.getInstance().getxBlocks(), WindowManager.getInstance().getBlockSize()*WindowManager.getInstance().getyBlocks());
 		iv.setImage(frames[0]);
 		Input.getInstance().setBlockInputs(true);
 	}
